a9d4007325d7699d1a2fd5c44bc8cdd4fe37ae4b
JiJibrto <jijibrto@gmail.com>
2020-10-18 23:53:15 +0300

    Add solution for individual task

---
+++ individual.py
@@ -0,0 +1,12 @@
+
+
+# init
+mon = int(input("Enter monitor cost> "))
+sys = int(input("Enter system unit cost> "))
+key = int(input("Enter keyboard cost> "))
+mou = int(input("Enter mouse cost> "))
+num = int(input("Enter PC's number> "))
+
+# out
+print("\nAll cost = %i$" % ((mon + sys + key + mou) * num))
+
