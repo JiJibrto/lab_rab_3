e98247330a19bd5e43c9e96dfd39de7c96855c1e
JiJibrto <jijibrto@gmail.com>
2020-10-18 23:07:40 +0300

    Add solution for first task

---
+++ user.py
@@ -0,0 +1,8 @@
+
+#initialization and input
+name = input("Enter your name> ")
+age = input("How old are you?> ")
+live = input("Where are you live?> ")
+
+#output
+print("This is %s\nIt is %s\n(S)he live in %s" % (name, age, live))
