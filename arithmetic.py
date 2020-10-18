d89132078a6d786456d39d636362247f3bab519b
JiJibrto <jijibrto@gmail.com>
2020-10-18 23:22:59 +0300

    Add solution for task 2 and some changes to user.py

---
+++ arithmetic.py
@@ -0,0 +1,8 @@
+
+# init
+print("Try to solve 4*100-54=y")
+player_ans = input("y = ")
+
+# output
+print("\n4 * 100 - 54 = ", 4 * 100 - 54)
+print("Your answer = ", player_ans)
--- user.py
+++ user.py
@@ -1,8 +1,8 @@
 
-#initialization and input
+# initialization and input
 name = input("Enter your name> ")
 age = input("How old are you?> ")
 live = input("Where are you live?> ")
 
-#output
+# output
 print("This is %s\nIt is %s\n(S)he live in %s" % (name, age, live))
